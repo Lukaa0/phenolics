b31bf53572cade07ac53a4589eeebb57a563f07d
Pierre <Pierre@DESKTOP-D3PPC01>
2020-11-24 20:34:07 +0100

    added some test for phenolic

--- phenolic_main/src/test/java/pt/ipb/phenolic/repos/PhenolicRepositoryTests.java
+++ phenolic_main/src/test/java/pt/ipb/phenolic/repos/PhenolicRepositoryTests.java
@@ -10,8 +10,14 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import pt.ipb.phenolic.PhenolicApplication;
+import pt.ipb.phenolic.models.Molecule;
 import pt.ipb.phenolic.models.Phenolic;
 import pt.ipb.phenolic.repos.PhenolicRepository;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest(classes = {PhenolicApplication.class})
@@ -26,17 +32,89 @@ public class PhenolicRepositoryTests {
 
     @Test
     public void testCreatePhenolic() {
+        Set<Molecule> molecule = null;
+        Phenolic phenolic = new Phenolic();
+        phenolic.setId(0);
+        phenolic.setName("Testing");
+        phenolic.setMolecules(molecule);
 
+
+
+        phenolicRepository.save(phenolic);
+        assertNotNull(phenolic);
+        int id = phenolic.getId();
+        String name = phenolic.getName();
+
+
+        assertEquals(id, 0);
+        assertEquals(name, "Testing");
+        assertEquals(phenolic.getMolecules(), molecule);
 
     }
     @Test
     public void testGetPhenolic() {
+        Set<Molecule> molecule = null;
+        Phenolic phenolic = new Phenolic();
+        phenolic.setId(0);
+        phenolic.setName("Testing");
+        phenolic.setMolecules(molecule);
+
+
+
+        phenolicRepository.save(phenolic);
+        assertNotNull(phenolic);
+        int id = phenolic.getId();
+        String name = phenolic.getName();
+
+
+        assertEquals(id, 0);
+        assertEquals(name, "Testing");
+        assertEquals(phenolic.getMolecules(), molecule);
+
     }
     @Test
     public void testUpdatePhenolic() {
+        Set<Molecule> molecule = null;
+        Phenolic phenolic = new Phenolic();
+        phenolic.setId(0);
+        phenolic.setName("Testing");
+        phenolic.setMolecules(molecule);
+
+
+
+        phenolicRepository.save(phenolic);
+        assertNotNull(phenolic);
+        int id = phenolic.getId();
+        String name = phenolic.getName();
+
+
+        assertEquals(id, 0);
+        assertEquals(name, "Testing");
+        assertEquals(phenolic.getMolecules(), molecule);
+
     }
     @Test
     public void testDeletePhenolic() {
+        Set<Molecule> molecule = null;
+        Phenolic phenolic = new Phenolic();
+        phenolic.setId(0);
+        phenolic.setName("Testing");
+        phenolic.setMolecules(molecule);
+
+
+
+        phenolicRepository.save(phenolic);
+        assertNotNull(phenolic);
+        int id = phenolic.getId();
+        String name = phenolic.getName();
+
+
+        assertEquals(id, 0);
+        assertEquals(name, "Testing");
+        assertEquals(phenolic.getMolecules(), molecule);
+
+
+
     }
 
 }
