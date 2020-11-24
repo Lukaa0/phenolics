9d978b80fdf522e1d3aed7ceca95575913861560
Pierre <Pierre@DESKTOP-D3PPC01>
2020-11-24 19:04:12 +0100

    Added the  4 Tests classes to diferrenciate them

---
+++ phenolic_api/src/test/java/pt/ipb/phenolic/FoodRepositoryTests.java
@@ -0,0 +1,18 @@
+package pt.ipb.phenolic;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FoodRepositoryTests {
+
+    // I should test for al 4 Repositorys
+    // Having
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
---
+++ phenolic_api/src/test/java/pt/ipb/phenolic/MoleculeRepositoryTests.java
@@ -0,0 +1,16 @@
+package pt.ipb.phenolic;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MoleculeRepositoryTests {
+
+    // I should test for al 4 Repositorys
+    // Having
+    @Test
+    void contextLoads() {
+    }
+
+}
