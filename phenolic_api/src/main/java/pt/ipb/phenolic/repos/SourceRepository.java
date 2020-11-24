123365ddd3bb98425195c3a860adc40c15c5e750
Pierre <Pierre@DESKTOP-D3PPC01>
2020-11-24 20:14:54 +0100

    Bug fixed wth Testing not executing correctly Still have a bug we have to execute the Tests in the Main..

--- phenolic_api/src/main/java/pt/ipb/phenolic/repos/SourceRepository.java
+++ phenolic_api/src/main/java/pt/ipb/phenolic/repos/SourceRepository.java
@@ -1,7 +1,8 @@
 package pt.ipb.phenolic.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pt.ipb.phenolic.models.Source;
-
+@Repository
 public interface SourceRepository extends JpaRepository<Source, Integer> {
 }
