0e69ee5bc301023bec5df33f96bb275ed03afb56
Leonardo Guth <leoguth55@hotmail.com>
2021-01-13 13:59:40 +0000

    Update made in LamdasRequest/MSFragmentRequest/MoleculesRequest/Relative/MoleculeController

--- phenolic_api/src/main/java/pt/ipb/phenolic/models/Relative.java
+++ phenolic_api/src/main/java/pt/ipb/phenolic/models/Relative.java
@@ -1,6 +1,9 @@
 package pt.ipb.phenolic.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import pt.ipb.phenolic.dtos.LambdaRequest;
+import pt.ipb.phenolic.dtos.MSFragmentRequest;
+
 import java.util.Set;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -8,6 +11,10 @@ public class Relative {
     private String type;
 
     private Set<Value> values;
+
+    private Set<MSFragmentRequest> msfragments;
+
+    private Set<LambdaRequest> lambdas;
 
     public String getType() {
         return type;
@@ -25,15 +32,31 @@ public class Relative {
         this.values = values;
     }
 
+    public Relative (String type, Set<Value> values){
+        this.type = type;
+        this.values = values;
+    }
+
+    public Set<MSFragmentRequest> getMsfragments() {
+        return msfragments;
+    }
+
+    public void setMsfragments(Set<MSFragmentRequest> msfragments) {
+        this.msfragments = msfragments;
+    }
+
+    public Set<LambdaRequest> getLambdas() {
+        return lambdas;
+    }
+
+    public void setLambdas(Set<LambdaRequest> lambdas) {
+        this.lambdas = lambdas;
+    }
+
     public Relative (){}
 
     public Relative (String type){
         this.type = type;
     }
 
-
-    public Relative (String type, Set<Value> values){
-        this.type = type;
-        this.values = values;
-    }
 }
